b7c4849f2bb89d5f7ef4c8122d0818721ce48144
rahulverma117 <117rahulverma@gmail.com>
2021-02-09 10:14:50 -0600

    Update Home.js

--- FractalExcelWebAddInWeb/Home.js
+++ FractalExcelWebAddInWeb/Home.js
@@ -1186,9 +1186,6 @@ async function calcSiteLife(year) {
         tempMonTable3Range = "VR" + (5 + (year -1) * 9).toString() + ":" + "WC" + (5 + (year -1) * 9 + 8).toString();
         tempMonTable3 = outSheet.getRange(tempMonTable3Range);
         tempMonTable3.load("values");
-        // Set calculation mode to manual
-        var app = context.workbook.application;
-        set(calculationMode: Excel.CalculationMode = "Manual");
         
         await context.sync();
         {
